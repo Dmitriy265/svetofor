5cb19660eba4177d883340a5a67be75989839f04
Dmitriy265 <63237845+Dmitriy265@users.noreply.github.com>
2020-04-10 12:36:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Светофор</title></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Светофор</h1>
+  <div id="box">
+   <div class="red"></div>
+   <div class="yellow"></div>
+   <div class="green"></div>
+  </div>
+</body>
+</html>
+
